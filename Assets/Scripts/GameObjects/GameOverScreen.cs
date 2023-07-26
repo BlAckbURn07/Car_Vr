1e8523a7572a5165fc58575eaa1a06a7383ef62c
BlAckbURn07 <harshjaiswalcr7@gmail.com>
2023-07-26 22:50:27 +0530

    first commit

---
+++ Assets/Scripts/GameObjects/GameOverScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public TextMeshProUGUI textMesh;
+
+    public void UpdateScoreTXT(float score)
+    {
+        textMesh.text = score.ToString();
+    }
+
+}
